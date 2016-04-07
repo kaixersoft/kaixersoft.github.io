abc3e53f7d809a291c2f739f1929bfef45dd4a93
xerxis <kaixersoft@gmail.com>
2016-04-07 08:32:30 +0800

    Copy changes, added nav click events

--- www/js/landing.js
+++ www/js/landing.js
@@ -1,3 +1,26 @@
+/* TooTip */
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
+});
+
+
+/* Pages */
+$('.nav-box .fa-smile-o').click(function(){
+	location.href = "/about.html";
+});
+
+$('.nav-box .fa-flag-checkered').click(function(){
+	location.href = "/portfolio.html";
+});
+
+$('.nav-box .fa-puzzle-piece').click(function(){
+	location.href = "/technology.html";
+});
+
+$('.nav-box .fa-wordpress').click(function(){
+	location.href = "/blog.html";
+});
+
+$('.nav-box .fa-gift').click(function(){
+	location.href = "/gift.html";
 });
